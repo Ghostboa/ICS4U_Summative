cb353217363696c0b3abeffa7430a20906ffc5fc
NinjaSnowman66 <alex.wilson667@gmail.com>
2015-01-10 18:45:04 -0500

    Holy Shitamoli, it mostly works!
    Aside from aces not being handled yet.  And the fact that there's no
    actual turn structure yet. But it's probably 70% of the way to bare
    bones done. (Not counting bugs. and file reading stuffs.)

--- Blackjack beginning.cpp
+++ Blackjack beginning.cpp
@@ -6,10 +6,11 @@ whether or not it's in play.*/
 
 // Blackjack beginning.cpp : Defines the entry point for the console application.
 
-//Requirements: Save output to File, Recursive shuffle, complete actual game with Dealer AI
+//Requirements: Save output to File, Recursive shuffle, complete actual game with Dealer AI. 
 // Other cool stuff: Variable AI aggressiveness/Number of Players, number of cards, game records or login thing, actual betting.
 
-// There is currently a lot of test printfs which should maybe be removed?
+
+//IMPORTANT NOTE: ACES ARE NOT HANDLED CORRECTLY YET!
 
 //#include "stdafx.h"
 #include <stdio.h>
@@ -22,16 +23,17 @@ int const maxCards = 52;
 
 enum suits { Hearts, Diamonds, Clubs, Spades };
 struct card{
-	int value; // Goes from 1 to 13
-	int counter; // Goes from 1 to 10
+	int value ; // Goes from 1 to 13
+	int counter ; // Goes from 1 to 10
 	suits suit;
 };
 
 struct hand { // Place where the players keep their cards. The count is the thing going up to 21.
 	//This was giving me errors unless I DID NOT initialize them (ISO C++ forbids initialization of member 'count')
 	card playerCard[10];
-	int handCounter = 0;;
-	int numCards = 0;;
+	int handCounter = 0;
+	int numCards = 0;
+	bool bust = false;
 };
 
 
@@ -63,10 +65,13 @@ void swapCards(card *card1, card *card2){ // A swap. For cards. this is the hack
 	card tempCard;
 	tempCard.value = card1[0].value;
 	tempCard.suit = card1[0].suit;
+	tempCard.counter = card1[0].counter;
 	card1[0].value = card2[0].value;
 	card1[0].suit = card2[0].suit;
+	card1[0].counter = card2[0].counter;
 	card2[0].value = tempCard.value;
 	card2[0].suit = tempCard.suit;
+	card2[0].counter = tempCard.counter;
 
 
 }
@@ -90,7 +95,7 @@ void startCards(card *cards){ // Function that initializes all of the cards.
 		num++;
 	}
 	for (int i = 0; i < 52; i++)
-		if (cards[i].value <= 10)
+		if (cards[i].value <= 10 )
 			cards[i].counter = cards[i].value;
 		else
 			cards[i].counter = 10;
@@ -99,19 +104,19 @@ void startCards(card *cards){ // Function that initializes all of the cards.
 void printCard(card *cards){ // Function that prints cards. It doesn't quite do jack/queen/king yet. Oh well.
 	switch (cards[0].value){
 	case (1) :
-		printf("A%c\n", cards[0].suit + 3);
+		printf("A%c ", cards[0].suit + 3);
 		break;
 	case (11) :
-		printf("J%c\n", cards[0].suit + 3);
+		printf("J%c ", cards[0].suit + 3);
 		break;
 	case (12) :
-		printf("Q%c\n", cards[0].suit + 3);
+		printf("Q%c ", cards[0].suit + 3);
 		break;
 	case (13) :
-		printf("K%c\n", cards[0].suit + 3);
+		printf("K%c ", cards[0].suit + 3);
 		break;
 	default:
-		printf("%i%c\n", cards[0].value, cards[0].suit + 3);
+		printf("%i%c ", cards[0].value, cards[0].suit + 3);
 		break;
 	}
 
@@ -140,35 +145,101 @@ void shuffle(card *cards){ // The weird shuffle thing. This will be recursive at
 
 }
 
-void hit(card *cards, hand *hands, int players, int topDeck){
+int hit(card *cards, hand *hands, int topDeck,int playerNum){ // Deals the player a card.
+	int cardNum = hands[playerNum].numCards;
 
+	hands[playerNum].playerCard[cardNum].value = cards[topDeck].value;
+	hands[playerNum].playerCard[cardNum].suit = cards[topDeck].suit;
+	hands[playerNum].playerCard[cardNum].counter = cards[topDeck].counter;
+	hands[playerNum].handCounter += hands[playerNum].playerCard[cardNum].counter;
 
+	hands[playerNum].numCards++;
+	topDeck++;
+
+	if (hands[playerNum].handCounter > 21) // Checks for the count being over 21.
+		hands[playerNum].bust = true;
+
+	return topDeck;
 }
+
+
 
 int deal(card *cards, hand *hands, int players, int topDeck){ //Gives cards out to all the players in the game.Topdeck is supposed to be a way of referencing the top of the "stack", or deck.
 	for (int i = 0; i <= players - 1; i++){				   // The way topDeck is handled is currently pretty bad and should be improved.
-		hands[i].playerCard[0].value = cards[topDeck].value;
-		hands[i].playerCard[0].suit = cards[topDeck].suit;
-		topDeck++;
-		hands[i].playerCard[1].value = cards[topDeck].value;
-		hands[i].playerCard[1].suit = cards[topDeck].suit;
-		topDeck++;
+		topDeck = hit(cards, hands, topDeck, i);
+		topDeck = hit(cards, hands, topDeck, i);
 	}
 
+	return topDeck;
+}
+
+int playerTurn(card *cards, hand *hands, int players, int topDeck){ // TBD
+
+	return topDeck;
+}
+
+int AITurn(card *cards, hand *hands, int players, int topDeck){ // TBD
+
+	return topDeck;
+}
+
+void display(card *cards, hand *hands, int players){ // THIS IS IMPORTANT. Wilson sucks at anything requiring any sense of aesthetics. This displays the stuff. -Dorien 2015.
+	for (int i = 0; i <= players - 1; i++){
+		printf("Player %i:", i+1);
+
+		for (int k = 0; k < hands[i].numCards;k++){
+			printCard(&hands[i].playerCard[k]);
+		}
+		printf("\n");
+
+		printf("Player %i count: %i. \n\n", i+1, hands[i].handCounter);
+	}
+}
+
+bool ended(hand *hands, int players){ // Function that checks to see if all hands are busted.
+	for (int i = 0; i < players; i++){
+		if (hands[i].bust == false)
+			return false;
+	}
+
+	return true; // Will only return true if all hands are busted...
+}
+
+int turns(card *cards, hand *hands, int players, int topDeck){ // Visual studio is bugging me about not all paths returning...
+																// This will run turns until the game is done.
+
+	for (int i = 0; i <= players - 1; i++){
+		display(cards,hands,players);
+
+		system("PAUSE");
+
+		if (i == 0 && hands[i].bust == false)
+			topDeck = playerTurn(cards,hands,players,topDeck);
+		else if (i != 0 && hands[i].bust == false)
+			topDeck = AITurn(cards, hands, players, topDeck);
+	}
+
+	if (ended(hands,players) == true)
 	return topDeck;
 }
 
 int startGame(card *cards){ // Screw non-dealer AI for now, just the dealer and the player...
 	hand hands[4];
 	int topDeck = 0;		// The master game thing which calls all the other game functions.
+	bool end = false;
+	int players = 2;
 
 	shuffle(&cards[0]);
 	printCard(&cards[0]);
 
-	topDeck = deal(cards, &hands[0], 2, topDeck);
+	topDeck = deal(cards, &hands[0], players, topDeck);
 	printf("Player one has ");
 	printCard(&hands[0].playerCard[0]);
 	printCard(&hands[0].playerCard[1]);
+	printf("\n");
+	printf("\n");
+
+		topDeck = turns(cards, &hands[0], players, topDeck);
 
 	printf("reached end of startgame\n");
 	system("PAUSE");
@@ -187,7 +258,6 @@ int main()
 	////////////////////////////////////////
 	srand(time(NULL)); // Seeding the thing
 	startCards(&cards[0]);
-	printCard(&cards[0]);
 	//we only need this once they start the game? otherwise it's just a waste?
 	////////////////////////////////////////
 
